1187be344d0335c98f7cd446d8675c2113447bd2
BhavanaReddy99 <jorrigalabhavana01@gmail.com>
2023-10-31 11:44:16 +0530

    Add project files.

---
+++ DoubleLinkedList/DoubleLinkedlist.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoublyLinkedList
+{
+    public class DoubleLinkedList
+    {
+        public Node Head { get; set; }
+        public Node Tail { get; set; }
+
+        public void Add(int data)
+        {
+            Node newNode = new Node(data);
+
+            if (Head == null)
+            {
+                Head = newNode;
+                Tail = newNode;
+            }
+            else
+            {
+                newNode.Previous = Tail;
+                Tail.Next = newNode;
+                Tail = newNode;
+            }
+        }
+
+        public void Display()
+        {
+            Node current = Head;
+            while (current != null)
+            {
+                Console.Write(current.Data + " ");
+                current = current.Next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ DoubleLinkedList/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoublyLinkedList
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            runApp();
+        }
+
+        static int CalculateLength(DoubleLinkedList list)
+        {
+            int length = 0;
+            Node current = list.Head;
+            while (current != null)
+            {
+                length++;
+                current = current.Next;
+            }
+            return length;
+        }
+        static void runApp()
+        {
+            DoubleLinkedList list = new DoubleLinkedList();
+
+            // Add elements to the doubly linked list using a for loop
+            for (int i = 10; i <= 20; i++)
+            {
+                list.Add(i);
+            }
+
+
+            // Calculate and display the length of the list
+            int length = CalculateLength(list);
+            Console.WriteLine("Length of the Doubly Linked List: " + length);
+            // Display the doubly linked list
+            Console.WriteLine("Doubly Linked List Elements:");
+            list.Display();
+
+            
+           
+        }
+
+       
+
+    }
+
+}
+    
